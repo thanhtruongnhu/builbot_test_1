75dac74a5753371aa42cc3841a728a73e2e7df6c
VN152-Encrypted <thanhtruong.kyle@gmail.com>
2019-03-11 10:55:45 +0700

    main

--- main/test_buildbot_1.py
+++ main/test_buildbot_1.py
@@ -1,8 +1,14 @@
 """Sample Test"""
 
-a = 1
-b = 1
-if a == b:
-    assert True
-print("finish")
+class Employee:
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + '.' + last + '@company.com'
+
+    def fullname(self):
+        return self.first + ' ' + self.last #'{} {}'.format(self.first,self.last)
+    def apply_raise(self):
+        self.pay = int(self.pay*1.04)
 
