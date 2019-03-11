4e25ce08c9737319466bba1c8ae68f75aa7c6fbd
VN152-Encrypted <thanhtruong.kyle@gmail.com>
2019-03-11 11:03:30 +0700

    main

--- main/test_buildbot_1.py
+++ main/test_buildbot_1.py
@@ -1,14 +1,8 @@
 """Sample Test"""
 
-class Employee:
-    def __init__(self, first, last, pay):
-        self.first = first
-        self.last = last
-        self.pay = pay
-        self.email = first + '.' + last + '@company.com'
+from hello import hello
 
-    def fullname(self):
-        return self.first + ' ' + self.last #'{} {}'.format(self.first,self.last)
-    def apply_raise(self):
-        self.pay = int(self.pay*1.04)
+a = "Truong"
+print(hello(a))
+print("Finished !")
 
